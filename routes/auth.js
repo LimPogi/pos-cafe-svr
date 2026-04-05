5af9252a1ec0fff3f6aa8e400828cc85c836e6d0
LimPogi <cirolimjoco@gmail.com>
2026-04-05 21:31:03 +0800

    fix backend updates

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authcontroller'); // Path to your controller
+const authController = require('./controllers/authcontroller'); // Path to your controller
 
 // This matches the frontend call: api.post('/auth/register')
 router.post('/register', authController.register);
